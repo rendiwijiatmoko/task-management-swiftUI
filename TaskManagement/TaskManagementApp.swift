8fc873adfdfea0f91915760fa45d68d689c5ca9e
Rendi Wijiatmoko <rendiwijiatmoko97@gmail.com>
2022-04-21 22:36:28 +0700

    Initial Commit

---
+++ TaskManagement/TaskManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagementApp.swift
+//  TaskManagement
+//
+//  Created by Rendi Wijiatmoko on 21/04/22.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
